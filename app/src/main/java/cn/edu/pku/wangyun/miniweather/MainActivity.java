99416d7ad22057d5ef87a8346c2bc06a91e85aa5
wangyun <wy1581052323@126.com>
2018-10-18 21:25:44 +0800

    ADD: NetUtil

--- app/src/main/java/cn/edu/pku/wangyun/miniweather/MainActivity.java
+++ app/src/main/java/cn/edu/pku/wangyun/miniweather/MainActivity.java
@@ -3,6 +3,10 @@ package cn.edu.pku.wangyun.miniweather;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.Toast;
+
+import cn.edu.pku.wangyun.util.NetUtil;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,14 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout);
+
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
+            Log.d("myWeather", "网络ok");
+            Toast.makeText(MainActivity.this, "网络ok", Toast.LENGTH_LONG).show();
+        } else {
+            Log.d("myWeather", "网络挂了");
+            Toast.makeText(MainActivity.this, "网络挂了", Toast.LENGTH_LONG).show();
+        }
     }
 
 }
